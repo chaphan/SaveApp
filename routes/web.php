26ee443ed150c72867d828ee931155e5913b7110
Jean Rene <jeanmrene8@gmail.com>
2018-12-09 00:15:53 +0200

    -- edited web

--- routes/web.php
+++ routes/web.php
@@ -16,38 +16,22 @@ Route::get('/', function () {
 });
 
 Route::get('/members', 'Members@show');
-<<<<<<< HEAD
-Route::post('/members/create', 'Members@create');
-=======
 Route::get('/members/create', 'Members@create');
->>>>>>> 4f5d4e7affde775fea983d68714efab6150ab35a
 Route::post('/members/save', 'Members@store');
 
 
 Route::get('/groups', 'Groups@show');
-<<<<<<< HEAD
-Route::post('/groups/create', 'Groups@create');
-=======
 Route::get('/groups/create', 'Groups@create');
->>>>>>> 4f5d4e7affde775fea983d68714efab6150ab35a
 Route::post('/groups/save', 'Groups@store');
 Route::get('/groups/{groupid}', 'Groups@show');
 
 Route::get('/savings/{groupid}', 'Savings@show');
-<<<<<<< HEAD
-Route::post('/savings/create', 'Savings@create');
-=======
 Route::get('/savings/create', 'Savings@create');
->>>>>>> 4f5d4e7affde775fea983d68714efab6150ab35a
 Route::post('/savings/save', 'Savings@store');
 
 
 
 Route::get('/products', 'Products@show');
-<<<<<<< HEAD
-Route::post('/products/create', 'Products@create');
-=======
 Route::get('/products/create', 'Products@create');
->>>>>>> 4f5d4e7affde775fea983d68714efab6150ab35a
 Route::get('/products/store', 'Products@store');
 Route::get('/products/{productid}', 'Products@show');
